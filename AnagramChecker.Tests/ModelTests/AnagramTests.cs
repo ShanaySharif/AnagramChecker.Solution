cd7e6a1f4aca87c4b1086cfa9b83737161365fa6
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-12 08:58:17 -0400

    second passing test

--- AnagramChecker.Tests/ModelTests/AnagramTests.cs
+++ AnagramChecker.Tests/ModelTests/AnagramTests.cs
@@ -12,10 +12,26 @@ namespace AnagramChecker.Tests
     {
 
         [TestMethod]
+    
         public void VocabConstructor_CreatesInstanceOfVocab_Vocab()
         {
             Vocab newVocab = new Vocab();
             Assert.AreEqual(typeof(Vocab), newVocab.GetType());
+        }
+
+        [TestMethod]
+        public void IsAnagram()
+
+        {
+          string inputtedWordOne = "Listen";
+          string inputtedWordTwo = "Silent";
+          Vocab newVocab = new Vocab();
+
+          //Assert
+          bool result = newVocab.IsAnagram(inputtedWordOne, inputtedWordTwo);
+
+          Assert.IsTrue(result);
+      
         }
     }
 
--- AnagramChecker/Models/Anagram.cs
+++ AnagramChecker/Models/Anagram.cs
@@ -17,6 +17,10 @@ namespace AnagramChecker.Models
             return wordOneChars.SequenceEqual(wordTwoChars);
             
         }
+
+
+        
     }
+
 }
 
